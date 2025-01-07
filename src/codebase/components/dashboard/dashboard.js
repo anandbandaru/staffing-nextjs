3997142d0c0e6269d7bad9a2673f7e6001f68700
anandbandaru <anand.bandaru@gmail.com>
2025-01-06 18:39:28 -0600

    separated master data cards to bottom + vendors count

--- src/codebase/components/dashboard/dashboard.js
+++ src/codebase/components/dashboard/dashboard.js
@@ -19,13 +19,14 @@ const Dashboard = () => {
         employees: 0,
         clients: 0,
         implementationpartners: 0,
+        vendors: 0,
         jobs: 0,
         invoices: 0,
         payroll: 0,
         timesheets: 0,
         filetypes: 0,
         expensetypes: 0,
-        jobtypes:0,
+        jobtypes: 0,
         files: 0,
         storageusage: 0,
         storagelimit: 0,
@@ -44,6 +45,7 @@ const Dashboard = () => {
                 '/counts/employees',
                 '/counts/clients',
                 '/counts/implementationpartners',
+                '/counts/vendors',
                 // '/counts/jobs',
                 // '/counts/invoices',
                 // '/counts/payroll',
@@ -98,105 +100,82 @@ const Dashboard = () => {
 
     return (
         <>
-            <div className="my-10">
-
-            </div>
-            <Grid container spacing={1} className='p-5 mt-10 bg-slate-200'>
-                <Grid item md={1}>
-                    {renderCard('Owners', counts.owners)}
-                </Grid>
-                <Grid item md={1.6}>
-                    <Card sx={{ minWidth: 155 }}>
-                        <CardContent>
-                            <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 12 }}>
-                                To Dos
-                            </Typography>
-                            <Stack spacing={1} direction="row">
-                                {apiLoading ? (
-                                    <Skeleton variant="rectangular" width={210} height={20} />
-                                ) : (
-                                    <>
-                                        <div className='dashCardsCounts'>{counts.todos}</div>
-                                        <div title='Active' className='dashCardsCounts bg-green-500 px-2'>{counts.activetodos}</div>
-                                        <div title='Completed' className='dashCardsCounts bg-yellow-400 px-2'>{counts.completedtodos}</div>
-                                        <div title='Active & Important' className='bg-red-500 px-2 text-white dashCardsCounts'>{counts.importantactivetodos}</div>
-                                    </>
-                                )}
-                            </Stack>
-                        </CardContent>
-                        {/* <CardActions>
+            <div className="my-10 mx-3">
+                <Grid container spacing={1} className='p-5 mt-10 bg-slate-200'>
+                    <Grid item md={1}>
+                        {renderCard('Owners', counts.owners)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('Companies', counts.companies)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('employees', counts.employees)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('Clients', counts.clients)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('Impl. Partners', counts.implementationpartners)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('Vendors', counts.vendors)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('Jobs', counts.jobs)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('Invoices', counts.invoices)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('Payroll', counts.payroll)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('Timesheets', counts.timesheets)}
+                    </Grid>
+                    <Grid item md={1}>
+                        {renderCard('Files', counts.files)}
+                    </Grid>
+                    <Grid item md={1.6}>
+                        <Card sx={{ minWidth: 155 }}>
+                            <CardContent>
+                                <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 12 }}>
+                                    To Dos
+                                </Typography>
+                                <Stack spacing={1} direction="row">
+                                    {apiLoading ? (
+                                        <Skeleton variant="rectangular" width={210} height={20} />
+                                    ) : (
+                                        <>
+                                            <div className='dashCardsCounts'>{counts.todos}</div>
+                                            <div title='Active' className='dashCardsCounts bg-green-500 px-2'>{counts.activetodos}</div>
+                                            <div title='Completed' className='dashCardsCounts bg-yellow-400 px-2'>{counts.completedtodos}</div>
+                                            <div title='Active & Important' className='bg-red-500 px-2 text-white dashCardsCounts'>{counts.importantactivetodos}</div>
+                                        </>
+                                    )}
+                                </Stack>
+                            </CardContent>
+                            {/* <CardActions>
                         Something here
                     </CardActions> */}
-                    </Card>
+                        </Card>
+                    </Grid>
+                    <Grid item md={2}>
+                    </Grid>
                 </Grid>
-                <Grid item md={1}>
-                    {renderCard('Companies', counts.companies)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('employees', counts.employees)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('Clients', counts.clients)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('Impl. Partners', counts.implementationpartners)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('Jobs', counts.jobs)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('Invoices', counts.invoices)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('Payroll', counts.payroll)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('Timesheets', counts.timesheets)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('File Types', counts.filetypes)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('Expense Types', counts.expensetypes)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('Job Types', counts.jobtypes)}
-                </Grid>
-                <Grid item md={1}>
-                    {renderCard('Files', counts.files)}
-                </Grid>
-                <Grid item md={2}>
-                </Grid>
-            </Grid>
-            <div className='mt-4 flex-0 p-5 bg-slate-200' >
-                <Card sx={{ maxWidth: 280 }} >
-                    <CardContent className='mt-0'>
-                        <Stack className='mt-0' spacing={2} direction={"row"}>
-                            <div className='flex justify-center items-center border-r-2 border-red-500 pr-2'>
-                                <AddToDriveOutlinedIcon fontSize='large' className="h-36" />
-                            </div>
-                            <div className='pr-2'>
-                                <Typography component="div">
-                                    Google Drive Storage Utilization
-                                </Typography>
-                                {apiLoading
-                                    ?
-                                    <Skeleton variant="circular" width={20} height={20} />
-                                    :
-                                    <>
-                                        <Stack className='mt-3' spacing={2} direction={"row"}>
-                                            <Chip label={`LIMIT: ${counts.storagelimit} GB`} color="primary" variant="outlined" size="small"></Chip>
-                                            <Chip label={`USAGE: ${counts.storageusage.toFixed(2)} GB`} color="error" variant="outlined" size="small"></Chip>
-                                        </Stack>
-                                        <Box mt={1} spacing="2">
-                                            <LinearProgress className='gDriveProgress' variant="determinate" value={(counts.storageusage / counts.storagelimit) * 100} />
-                                        </Box>
-                                    </>
-                                }
-                            </div>
-                        </Stack>
-                    </CardContent>
-                </Card>
+                <div className='mt-4 flex-0  bg-slate-200' >
+                    <Grid container spacing={1} className='p-5 mt-10 bg-slate-200'>
+                        <Grid item md={1}>
+                            {renderCard('File Types', counts.filetypes)}
+                        </Grid>
+                        <Grid item md={1}>
+                            {renderCard('Expense Types', counts.expensetypes)}
+                        </Grid>
+                        <Grid item md={1}>
+                            {renderCard('Job Types', counts.jobtypes)}
+                        </Grid>
+                    </Grid>
+                </div>
+
             </div>
         </>
     );
