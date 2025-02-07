37158c5b7bf8146dbb31213c4fc121244fd0b2a9
anandbandaru <anand.bandaru@gmail.com>
2025-02-07 12:57:33 -0600

    Sent reminders & reminders capture in DB

--- src/codebase/components/timesheetentry/pendingCards.js
+++ src/codebase/components/timesheetentry/pendingCards.js
@@ -16,10 +16,12 @@ import TimesheetEntryMetadata from './timesheetentryMetadata';
 import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 import TimesheetAudit from '../timesheets/timesheetAudit';
 import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+import NotificationAddIcon from '@mui/icons-material/NotificationAdd';
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import CustomSnackbar from "../snackbar/snackbar";
 import axios from 'axios';
 import emailjs from 'emailjs-com';
+import TimesheetReminders from '../timesheets/timesheetReminders';
 
 const TimesheetEntryForm = ({ data, onFormSubmitSuccess, mode }) => {
     const { APIPath, userName } = useContext(Context);
@@ -54,60 +56,95 @@ const TimesheetEntryForm = ({ data, onFormSubmitSuccess, mode }) => {
     };
 
     const sendReminder = (tn, eid, tf, tt, tjn, pemail) => {
-        console.log(tn);
-        console.log(eid);
-        console.log(tf);
-        console.log(tt);
-        console.log(tjn);
+        if (window.confirm(`Are you sure you want to send reminder?`)) {
+            console.log(tn);
+            console.log(eid);
+            console.log(tf);
+            console.log(tt);
+            console.log(tjn);
 
-        // Create a virtual form element
-        const form = document.createElement('form');
+            // Create a virtual form element
+            const form = document.createElement('form');
 
-        // Create and append input elements to the form
-        const subjectInput = document.createElement('input');
-        subjectInput.setAttribute('type', 'hidden');
-        subjectInput.setAttribute('name', 'user_email');
-        subjectInput.setAttribute('value', pemail);
-        form.appendChild(subjectInput);
+            // Create and append input elements to the form
+            const subjectInput = document.createElement('input');
+            subjectInput.setAttribute('type', 'hidden');
+            subjectInput.setAttribute('name', 'user_email');
+            subjectInput.setAttribute('value', pemail);
+            form.appendChild(subjectInput);
 
-        const i_tn = document.createElement('input');
-        i_tn.setAttribute('type', 'hidden');
-        i_tn.setAttribute('name', 'tn');
-        i_tn.setAttribute('value', tn);
-        form.appendChild(i_tn);
+            const i_tn = document.createElement('input');
+            i_tn.setAttribute('type', 'hidden');
+            i_tn.setAttribute('name', 'tn');
+            i_tn.setAttribute('value', tn);
+            form.appendChild(i_tn);
 
-        const i_eid = document.createElement('input');
-        i_eid.setAttribute('type', 'hidden');
-        i_eid.setAttribute('name', 'eid');
-        i_eid.setAttribute('value', eid);
-        form.appendChild(i_eid);
+            const i_eid = document.createElement('input');
+            i_eid.setAttribute('type', 'hidden');
+            i_eid.setAttribute('name', 'eid');
+            i_eid.setAttribute('value', eid);
+            form.appendChild(i_eid);
 
-        const i_tf = document.createElement('input');
-        i_tf.setAttribute('type', 'hidden');
-        i_tf.setAttribute('name', 'tf');
-        i_tf.setAttribute('value', tf);
-        form.appendChild(i_tf);
+            const i_tf = document.createElement('input');
+            i_tf.setAttribute('type', 'hidden');
+            i_tf.setAttribute('name', 'tf');
+            i_tf.setAttribute('value', tf);
+            form.appendChild(i_tf);
 
-        const i_tt = document.createElement('input');
-        i_tt.setAttribute('type', 'hidden');
-        i_tt.setAttribute('name', 'tt');
-        i_tt.setAttribute('value', tt);
-        form.appendChild(i_tt);
+            const i_tt = document.createElement('input');
+            i_tt.setAttribute('type', 'hidden');
+            i_tt.setAttribute('name', 'tt');
+            i_tt.setAttribute('value', tt);
+            form.appendChild(i_tt);
 
-        const i_tjn = document.createElement('input');
-        i_tjn.setAttribute('type', 'hidden');
-        i_tjn.setAttribute('name', 'tjn');
-        i_tjn.setAttribute('value', tjn);
-        form.appendChild(i_tjn);
+            const i_tjn = document.createElement('input');
+            i_tjn.setAttribute('type', 'hidden');
+            i_tjn.setAttribute('name', 'tjn');
+            i_tjn.setAttribute('value', tjn);
+            form.appendChild(i_tjn);
 
-        emailjs.sendForm('service_68p81qk', 'template_ekt8k16', form, 'r6Fya2Opl9134qi3r')
-            .then((result) => {
-                showSnackbar('success', "Reminder email sent to: " + pemail);
-                console.log("sendReminder");
-            }, (error) => {
-                showSnackbar('error', "Error sending email to user");
-            });
+            emailjs.sendForm('service_68p81qk', 'template_ekt8k16', form, 'r6Fya2Opl9134qi3r')
+                .then((result) => {
+                    showSnackbar('success', "Reminder email sent to: " + pemail);
+                    console.log("sendReminder");
+                    SendReminder(tn);
+                }, (error) => {
+                    showSnackbar('error', "Error sending email to user");
+                });
+        }
     };
+
+    const SendReminder = (timesheetNumber) => {
+
+        let apiUrl = APIPath + "/sendreminder";
+        axios.post(apiUrl,
+            {
+                timesheetNumber: timesheetNumber,
+                reminderSentBy: userName
+            },
+            {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json',
+                }
+            },
+        ).then(
+            (result) => {
+                if (result.error) {
+                    showSnackbar('error', "Error occurred while saving reminder data");
+                } else {
+                    if (result.data.STATUS === "FAIL") {
+                        showSnackbar('error', result.data.ERROR.MESSAGE);
+                    } else {
+                        showSnackbar('success', "Reminder data modified.");
+                    }
+                }
+            },
+            (error) => {
+                showSnackbar('error', "Error occurred while saving reminder data");
+            }
+        )
+    }
 
     //For dialog MUI
     const Transition = React.forwardRef(function Transition(props, ref) {
@@ -243,8 +280,9 @@ const TimesheetEntryForm = ({ data, onFormSubmitSuccess, mode }) => {
                                                                                         )
                                                                                     }
                                                                                 >
-                                                                                    <NotificationsActiveOutlinedIcon />
+                                                                                    <NotificationAddIcon />
                                                                                 </IconButton>
+                                                                                <TimesheetReminders timesheetNumber={timesheet.timesheetNumber} />
                                                                             </>
                                                                         )}
                                                                     </div>
---
+++ src/codebase/components/timesheets/timesheetReminders.js
@@ -0,0 +1,95 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import Popover from '@mui/material/Popover';
+import { styled } from '@mui/material/styles';
+import CloseIcon from '@mui/icons-material/Close';
+import { Context } from "../../context/context";
+import NewReleasesOutlinedIcon from '@mui/icons-material/NewReleasesOutlined';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
+
+function TimesheetReminders({ timesheetNumber }) {
+    const { APIPath, userName } = useContext(Context);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [data, setData] = useState('');
+    const [apiLoading, setApiLoading] = useState('');
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+    useEffect(() => {
+        getReminders();
+    }, [timesheetNumber]);
+
+    const getReminders = () => {
+        setApiLoading(true);
+        let apiUrl = APIPath + "/gettimesheetreminders/" + timesheetNumber;
+        axios.get(apiUrl).then(
+            (result) => {
+                if (result.error) {
+
+                } else {
+                    if (result.data.STATUS === "FAIL") {
+
+                    } else {
+                        setData(result.data)
+                    }
+                }
+                setApiLoading(false);
+            },
+            (error) => {
+                setApiLoading(false);
+            }
+        )
+    }
+
+    return (
+        <>
+            <Stack direction="row" spacing={1} className=''>
+                <IconButton aria-label="PAST REMINDERS" title="PAST REMINDERS" color="primary" onClick={handleClick}>
+                    <NotificationsActiveOutlinedIcon />
+                </IconButton>
+            </Stack>
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+            >
+                <div style={{ padding: '10px' }}>
+                    <div className='reminderHistoryTitle'>Past Reminders:</div>
+                    {data && data.data.length > 0 ?
+                        data.data.map((item, index) => (
+                            <div key={index} value={item.Id} className='reminderHistoryRow'>
+                                {item.reminderSentDate} - {item.reminderSentBy}
+                            </div>
+                        )) :
+                        <>
+                            No reminders sent in the past
+                        </>
+                    }
+                </div>
+            </Popover>
+        </>
+    )
+}
+
+export default TimesheetReminders;
