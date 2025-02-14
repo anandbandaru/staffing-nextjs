70fcd00286a4b15f88e02cf6b85a140621dc04a3
anandbandaru <anand.bandaru@gmail.com>
2025-02-13 23:43:37 -0600

    UI small padding

--- src/codebase/components/employees/employeeJobsMetadata.js
+++ src/codebase/components/employees/employeeJobsMetadata.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Alert from '@mui/material/Alert';
 
 const EmployeeJobsMetadata = ({ employeeJobs }) => {
-
+    
     return (
         <div>
             {employeeJobs.length > 0 ? (
@@ -10,25 +10,25 @@ const EmployeeJobsMetadata = ({ employeeJobs }) => {
                     <table className="min-w-full bg-white border border-gray-200 text-left">
                         <thead>
                             <tr>
-                                <th className="py-2 px-4 border-b">Job Name</th>
-                                <th className="py-2 px-4 border-b">Job Title</th>
-                                <th className="py-2 px-4 border-b">Job Start</th>
-                                <th className="py-2 px-4 border-b">Job End</th>
-                                <th className="py-2 px-4 border-b">Client Name</th>
-                                <th className="py-2 px-4 border-b">Timesheets Period</th>
-                                <th className="py-2 px-4 border-b">Invoice Period</th>
+                                <th className="px-4 py-1 bg-slate-700 text-white">Job Name</th>
+                                <th className="px-4 bg-slate-700 text-white">Job Title</th>
+                                <th className="px-4 bg-slate-700 text-white">Job Start</th>
+                                <th className="px-4 bg-slate-700 text-white">Job End</th>
+                                <th className="px-4 bg-slate-700 text-white">Client Name</th>
+                                <th className="px-4 bg-slate-700 text-white">Timesheets Period</th>
+                                <th className="px-4 bg-slate-700 text-white">Invoice Period</th>
                             </tr>
                         </thead>
                         <tbody>
                             {employeeJobs.map(item => (
                                 <tr key={item.jobName}>
-                                    <td className="py-2 px-4 border-b border-r">{item.jobName}</td>
-                                    <td className="py-2 px-4 border-b border-r">{item.jobTitle}</td>
-                                    <td className="py-2 px-4 border-b border-r">{item.jobStartDate}</td>
-                                    <td className="py-2 px-4 border-b border-r">{item.jobEndDate}</td>
-                                    <td className="py-2 px-4 border-b border-r">{item.Client}</td>
-                                    <td className="py-2 px-4 border-b border-r">{item.timesheetsPeriod}</td>
-                                    <td className="py-2 px-4 border-b border-r">{item.invoicePeriod}</td>
+                                    <td className="py-1 px-4 border-b border-r">{item.jobName}</td>
+                                    <td className="py-1 px-4 border-b border-r">{item.jobTitle}</td>
+                                    <td className="py-1 px-4 border-b border-r">{item.jobStartDate}</td>
+                                    <td className="py-1 px-4 border-b border-r">{item.jobEndDate}</td>
+                                    <td className="py-1 px-4 border-b border-r">{item.Client}</td>
+                                    <td className="py-1 px-4 border-b border-r">{item.timesheetsPeriod}</td>
+                                    <td className="py-1 px-4 border-b border-r">{item.invoicePeriod}</td>
                                 </tr>
                             ))}
                         </tbody>
